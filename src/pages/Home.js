496d6305b1d7cffe4e9ae1b7ffdb0cf7a584f7fd
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2023-06-26 16:57:51 +0200

    ajustement 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,15 @@ import backgroundImg2 from "../assets/images/background2.png";
 
 const Home = () => {
   return (
-    <div className="home_class">
+    <div>
       <Header />
-      <img src={backgroundImg2} className="home_img" alt="limage d'un fond" />
-      <p className="textground">Chez vous, partout et ailleurs</p>
+      <div className="home_class">
+        <img src={backgroundImg2} className="home_img" alt="limage d'un fond" />
+        <p className="textground">Chez vous, partout et ailleurs</p>
 
-      <Gallery />
-      <Footer />
+        <Gallery />
+        <Footer />
+      </div>
     </div>
   );
 };
